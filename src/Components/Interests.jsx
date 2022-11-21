4f0212a77b34eebae776d2341fcc42e883e60e79
oolaoluwatobi <58662625+oolaoluwatobi@users.noreply.github.com>
2022-11-21 12:06:44 +0100

    Update Digital Business Card file

---
+++ src/Components/Interests.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Interests() {
+  return (
+    <div className='interests' >
+      <h3>Interests</h3>
+      <p>Food lover. Reader. Internet fanatic. Suya connoisseur. Music scholar. Travel enthusiast. Amateur astronomer. Video game geek.</p>
+    </div>
+  )
+}
+
+export default Interests
